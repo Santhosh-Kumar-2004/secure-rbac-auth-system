44e5a9e02d070d1ac27161920a6c3a027e569185
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-18 22:15:01 +0530

    Added the dependencies code imports

--- backend/core/dependencies.py
+++ backend/core/dependencies.py
@@ -1,1 +1,8 @@
 #Defines reusable dependencies (e.g., current user, DB session).
+from fastapi import Depends, HTTPException, Request, status
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+
+from database.database import get_db
+from models.user import User
+from core.security import SECRET_KEY, ALGORITHM
