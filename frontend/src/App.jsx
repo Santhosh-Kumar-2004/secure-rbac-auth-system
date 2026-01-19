ca46e0ef62d3fd772ada7c3afaeb6e8a4a4d9d36
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-19 13:03:58 +0530

    Updated the homepage router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           <Route path="/" element={
-            <ProtectedRoute allowedRoles={["admin"]}>
+            <ProtectedRoute allowedRoles={["admin", "user", "manager", "chef", "waiter"]}>
               <Home />
             </ProtectedRoute>
           } />
